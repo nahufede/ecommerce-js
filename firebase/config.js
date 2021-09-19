fb98b82a9f38597597d758a3a563354913b78126
nahufede <nahuelbarbieri.93@gmail.com>
2021-09-19 21:10:14 +0200

    mostrando firebase

---
+++ firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAiiMcRvX6TVL12YFmzx69BZCs2_Rf4PPA",
+    authDomain: "lucca-web.firebaseapp.com",
+    projectId: "lucca-web",
+    storageBucket: "lucca-web.appspot.com",
+    messagingSenderId: "1062412463715",
+    appId: "1:1062412463715:web:34b49ed73e20bfedd95256"
+  };
